cf1dcb7310786a73a79602f75e7d33a0b6ec0447
Rafaela Silverio <rafaelapetelin@gmail.com>
2025-02-15 15:03:20 -0300

    aula-5 finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Carrinhoprovider } from "./context/CarrinhoContext";
+import { CarrinhoProvider } from "./context/CarrinhoContext";
 import Home from "./pages/Home";
 import Carrinho from "./pages/Carrinho";
 import PaginaErro from "./pages/PaginaErro";
+
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Carrinhoprovider>
+      <CarrinhoProvider>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/carrinho" element={<Carrinho />} />
           <Route path="*" element={<PaginaErro />} />
         </Routes>
-      </Carrinhoprovider>
+      </CarrinhoProvider>
     </BrowserRouter>
   );
 }
